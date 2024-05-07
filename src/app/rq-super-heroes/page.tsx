9592a2b7bd4a8db29d54af3eaf8ad3452b20d810
MEGAPASTELITOS <megapastelitos@gmail.com>
2024-05-07 18:45:45 -0400

    feat: :sparkles: añadiendo las rutas del proyecto

---
+++ src/app/rq-super-heroes/page.tsx
@@ -0,0 +1,8 @@
+export default function SuperRQHoroesPage() {
+    return (
+      <>
+      Super heroes Page RQ
+      </>
+    );
+  }
+  
---
+++ src/app/super-heroes/page.tsx
@@ -0,0 +1,8 @@
+export default function SuperHoroesPage() {
+    return (
+      <>
+      Super heroes Page
+      </>
+    );
+  }
+  
